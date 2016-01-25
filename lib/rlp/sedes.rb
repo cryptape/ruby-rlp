e2a06ddbba1b047a8cedd6a4c1fef37453d94be1
Jan Xie <jan.h.xie@gmail.com>
2016-01-25 18:30:36 +0800

    fix Sedes.infer

--- lib/rlp/sedes.rb
+++ lib/rlp/sedes.rb
@@ -8,8 +8,19 @@ module RLP
   module Sedes
 
     class <<self
+      ##
+      # Try to find a sedes objects suitable for a given Ruby object.
+      #
+      # The sedes objects considered are `obj`'s class, `big_endian_int` and
+      # `binary`. If `obj` is a list, a `RLP::Sedes::List` will be constructed
+      # recursively.
+      #
+      # @param obj [Object] the Ruby object for which to find a sedes object
+      #
+      # @raise [TypeError] if no appropriate sedes could be found
+      #
       def infer(obj)
-        return obj if sedes?(obj)
+        return obj.class if sedes?(obj.class)
         return big_endian_int if obj.is_a?(Integer) && obj >= 0
         return binary if Binary.valid_type?(obj)
         return List.new(elements: obj.map {|item| infer(item) }) if RLP.list?(obj)
