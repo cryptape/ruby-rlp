3c21fd75c03610b8489f9b34c4ca7b93d73b1615
Jan Xie <jan.h.xie@gmail.com>
2016-01-28 20:56:20 +0800

    speed benchmark

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # ruby-rlp
 
 A Ruby implementation of Ethereum's Recursive Length Prefix encoding (RLP). You can find the specification of the standard in the [Ethereum wiki](https://github.com/ethereum/wiki/wiki/RLP).
+
+## Benchmark
+
+```
+ruby-rlp $ ruby -v -Ilib test/speed.py
+ruby 2.2.2p95 (2015-04-13 revision 50295) [x86_64-linux]
+Block serializations / sec: 2318.21
+Block deserializations / sec: 1704.61
+TX serializations / sec: 30461.76
+TX deserializations / sec: 21378.70
+
+pyrlp $ python -V
+Python 2.7.11
+
+pyrlp $ PYTHONPATH=. python tests/speed.py
+Block serializations / sec: 1225.00
+Block deserializations / sec: 1162.01
+TX serializations / sec: 16468.41
+TX deserializations / sec: 14517.31
+```
--- test/speed.rb
+++ test/speed.rb
@@ -12,10 +12,6 @@ $int256    = Sedes::BigEndianInt.new 256
 $hash32    = Sedes::Binary.fixed_length 32
 $trie_root = Sedes::Binary.fixed_length 32, allow_empty: true
 
-def zpad(x, l)
-  "#{BYTE_ZERO*[0, l-x.size].max}#{x}"
-end
-
 class Transaction
   include Sedes::Serializable
 
@@ -70,4 +66,123 @@ class Block
   def initialize(options)
     super({transaction_list: [], uncles: []}.merge(options))
   end
+end
+
+def zpad(x, l)
+  "#{BYTE_ZERO*[0, l-x.size].max}#{x}"
+end
+
+def rand_bytes(num=32)
+  zpad(Sedes.big_endian_int.serialize(rand(2**(num*8))), num)
+end
+alias rand_bytes32 rand_bytes
+
+def rand_address
+  rand_bytes(20)
+end
+
+def rand_bytes8
+  rand_bytes(8)
+end
+
+def rand_bigint
+  rand(2**256)
+end
+
+def rand_int
+  rand(2**32)
+end
+
+rand_map = {
+  $hash32 => rand_bytes32,
+  $trie_root => rand_bytes32,
+  Sedes.binary => rand_bytes32,
+  $address => rand_address,
+  Sedes::Binary => rand_bytes8,
+  Sedes.big_endian_int => rand_int,
+  $int256 => rand_bigint
+}
+
+def mk_transaction
+  Transaction.new(
+    nonce:    rand_int,
+    gasprice: rand_int,
+    startgas: rand_int,
+    to:       rand_address,
+    value:    rand_int,
+    data:     rand_bytes32,
+    v:        27,
+    r:        rand_bigint,
+    s:        rand_bigint
+  )
+end
+
+def mk_block_header
+  BlockHeader.new(
+    prevhash:      rand_bytes32,
+    uncles_hash:   rand_bytes32,
+    coinbase:      rand_address,
+    state_root:    rand_bytes32,
+    tx_list_root:  rand_bytes32,
+    receipts_root: rand_bytes32,
+    bloom:         rand_bigint,
+    difficulty:    rand_int,
+    number:        rand_int,
+    gas_limit:     rand_int,
+    gas_used:      rand_int,
+    timestamp:     rand_int,
+    extra_data:    rand_bytes32,
+    mixhash:       rand_bytes32,
+    nonce:         rand_bytes32
+  )
+end
+
+def mk_block(num_transactions=10, num_uncles=1)
+  Block.new(
+    header: mk_block_header,
+    transaction_list: (0...num_transactions).map {|i| mk_transaction },
+    uncles: (0...num_uncles).map {|i| mk_block_header }
+  )
+end
+
+def do_test_serialize(block, rounds=100)
+  x = nil
+  rounds.times do |i|
+    x = RLP.encode(block)
+  end
+  x
+end
+
+def do_test_deserialize(data, rounds=100, sedes=Block)
+  x = nil
+  rounds.times do |i|
+    x = RLP.decode(data, sedes: sedes)
+  end
+  x
+end
+
+def main(rounds=1000)
+  st = Time.now
+  d = do_test_serialize(mk_block, rounds)
+  elapsed = Time.now - st
+  puts "Block serializations / sec: %.2f" % (rounds/elapsed.to_f)
+
+  st = Time.now
+  d = do_test_deserialize(d, rounds)
+  elapsed = Time.now - st
+  puts "Block deserializations / sec: %.2f" % (rounds/elapsed.to_f)
+
+  st = Time.now
+  d = do_test_serialize(mk_transaction, rounds)
+  elapsed = Time.now - st
+  puts "TX serializations / sec: %.2f" % (rounds/elapsed.to_f)
+
+  st = Time.now
+  d = do_test_deserialize(d, rounds, Transaction)
+  elapsed = Time.now - st
+  puts "TX deserializations / sec: %.2f" % (rounds/elapsed.to_f)
+end
+
+if $0 =~ /speed.rb$/
+  main
 end
